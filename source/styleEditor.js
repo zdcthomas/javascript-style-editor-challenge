02a09e4732d93239bf964e534b4ea48c450231a5
zdcthomas <zdcthomas@yahoo.com>
2018-08-12 11:58:51 -0500

    implements the submit form thing

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  // your code goes here.
+  $('#style_editor').submit( (event) =>{
+    
+    let selector = document.getElementsByName('selector')[0].value;
+    let property = document.getElementsByName('property')[0].value;
+    let value = document.getElementsByName('value')[0].value;
+    
+    $(selector).css(property, value);
+    event.preventDefault();
+  })
 });
